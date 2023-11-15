69f35a114fc32b28627c9dca45d6277c3bbf2d4e
YOUNGJUNE <cerealmaster@naver.com>
2023-11-15 18:25:29 +0900

    changed to snake case for readability

--- test.cpp
+++ test.cpp
@@ -36,7 +36,7 @@ struct Node {
     int heuristic;
     Node* parent;
     //f(x) = g(x)+h(x)
-    int totalCost() const{
+    int total_cost() const{
         return cost+heuristic;
     }
 };
@@ -46,7 +46,7 @@ int heuristic(const Position& a, const Position& b) {
     return distance(a,b);
 }
 
-vector<Position> getNeighbors(const Position& a,const Board& board){ //get neighbored Node and store it in result
+vector<Position> get_neighbors(const Position& a,const Board& board){ //get neighbored Node and store it in result
     vector<Position> result = {};
     if (board.is_valid_position({a.row,a.column+1})) result.push_back({a.row,a.column+1});//0
     if (board.is_valid_position({a.row-1,a.column})) result.push_back({a.row-1,a.column});//1
@@ -57,16 +57,16 @@ vector<Position> getNeighbors(const Position& a,const Board& board){ //get neigh
 
 //return full paths to goal(0 is next position, last is goal)
 vector<Position> a_star(const Position& start, const Position& goal, const Board& board){
-    auto compare = [](const Node& a, const Node& b) { return a.totalCost() > b.totalCost(); }; //compare two nodes
-    std::priority_queue<Node, std::vector<Node>, decltype(compare)> openSet(compare); // this sets contains Nodes that needs to be treated, lowest cost comes on top
-    std::unordered_map<Position, Node> allNodes; //contains every Nodes that has or needs to be searched
+    auto compare = [](const Node& a, const Node& b) { return a.total_cost() > b.total_cost(); }; //compare two nodes
+    priority_queue<Node, vector<Node>, decltype(compare)> open_set(compare); // this sets contains Nodes that needs to be treated, lowest cost comes on top
+    unordered_map<Position, Node> all_nodes; //contains every Nodes that has or needs to be searched
 
-    openSet.push({start, 0, heuristic(start, goal),nullptr});//openset initial value(can't be empty unless search has done)  
-    allNodes[start] = {start, 0, heuristic(start, goal),nullptr};//allNodes initial value
+    open_set.push({start, 0, heuristic(start, goal),nullptr});//open_set initial value(can't be empty unless search has done)  
+    all_nodes[start] = {start, 0, heuristic(start, goal),nullptr};//all_nodes initial value
 
-    while (!openSet.empty()){
-        Node current = openSet.top();//current Node = top of the openset queue that has lowest totalcost
-        openSet.pop(); // delete the current node from openset since we don't need to iterate again
+    while (!open_set.empty()){
+        Node current = open_set.top();//current Node = top of the open_set queue that has lowest total_cost
+        open_set.pop(); // delete the current node from open_set since we don't need to iterate again
 
         if (current.Position == goal){ //when path search finished, go back to the parents Node(nullptr) and save every
         //Nodes in path vector. return REVERSED order of path since path[0] should be current position and last index to goal.
@@ -79,15 +79,15 @@ vector<Position> a_star(const Position& start, const Position& goal, const Board
             return path;
         }
 
-        for(const auto& neighbor : getNeighbors(current.Position, board)){
+        for(const auto& neighbor : get_neighbors(current.Position, board)){
             if(!board.is_valid_position(neighbor)) continue; //if the given neighbor is not valid, continue
             //!!!make node before adding in the unordered_map or priority queue. cause infinite loop for same adrees on Parent Node!!!
-            Node neighborNode = {neighbor, current.cost + 1, heuristic(neighbor,goal),&allNodes[current.Position]};
-            bool isNewNode = allNodes.find(neighbor) == allNodes.end();//check if it's not already in allNodes map
-            bool isCloserToGoal = isNewNode || (current.cost+1) < allNodes[neighbor].cost;//check new route is closer to goal
-            if(isCloserToGoal){ //if New Node is not in allNodes or have better costs
-                allNodes[neighbor] = neighborNode;//apppend new Nodes
-                openSet.push(neighborNode); //append new openSet
+            Node neighbor_node = {neighbor, current.cost + 1, heuristic(neighbor,goal),&all_nodes[current.Position]};
+            bool is_new_node = all_nodes.find(neighbor) == all_nodes.end();//check if it's not already in all_nodes map
+            bool is_closer_to_goal = is_new_node || (current.cost+1) < all_nodes[neighbor].cost;//check new route is closer to goal
+            if(is_closer_to_goal){ //if New Node is not in all_nodes or have better costs
+                all_nodes[neighbor] = neighbor_node;//apppend new Nodes
+                open_set.push(neighbor_node); //append new open_set
             }
         }
     }
